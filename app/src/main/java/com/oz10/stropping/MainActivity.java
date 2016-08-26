b15128738b198f386eda50a06005804804ea90e3
OZ10 <mostin@gmail.com>
2016-08-26 11:33:20 +0100

    SetupDatabase: Added more ingredients

--- app/src/main/java/com/oz10/stropping/MainActivity.java
+++ app/src/main/java/com/oz10/stropping/MainActivity.java
@@ -102,32 +102,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     
-    private void SetupDatabase()
-    {
-        StroppingDatabase stroppingDatabase = new StroppingDatabase(this);
-        stroppingDatabase.open();
-
-        stroppingDatabase.ResetAllTables();
-
-        stroppingDatabase = new StroppingDatabase(this);
-        stroppingDatabase.open();
-
-        ArrayList<Ingredient> ingredientArrayList = new ArrayList<>();
-
-        ingredientArrayList.add(stroppingDatabase.createIngredient("Red Pepper", "number of", 1, 1, 0, 0, 0, 0));
-        ingredientArrayList.add(stroppingDatabase.createIngredient("Garlic", "number of", 1, 1, 0, 0, 0, 0));
-        ingredientArrayList.add(stroppingDatabase.createIngredient("Red Onion", "number of", 1, 1, 0, 0, 0, 0));
-        ingredientArrayList.add(stroppingDatabase.createIngredient("Parsley", "grams", 10, 10, 0, 0, 0, 0));
-        ingredientArrayList.add(stroppingDatabase.createIngredient("Small Tomato", "number of", 1, 1, 0, 0, 0, 0));
-        ingredientArrayList.add(stroppingDatabase.createIngredient("Salad Onions", "number of", 6, 6, 0, 0, 0, 0));
-        ingredientArrayList.add(stroppingDatabase.createIngredient("Brown Rice", "grams", 250, 250, 0, 0, 0, 0));
-        ingredientArrayList.add(stroppingDatabase.createIngredient("Chicken Stock cube", "number of", 1, 1, 0, 0, 0, 0));
-        
-        stroppingDatabase.createRecipe("Spag", 2, "");
-        stroppingDatabase.createRecipe("Spicy Sausage Rice", 2, "", ingredientArrayList);
-        stroppingDatabase.createRecipe("Pasta", 2, "");
-    }
-
     public void mainFABClick(View view)
     {
         Fragment currentFragment = (Fragment) _pagerAdapter.GetFragmentByIndex(_pager.getCurrentItem());
@@ -203,6 +177,96 @@ public class MainActivity extends AppCompatActivity {
             RecipeListAdapter recipeListAdapter = recipesFragment._recipeListAdapter;
             recipeListAdapter.updateAdapterFromDatabase(this);
         }
+    }
+    
+        private void SetupDatabase()
+    {
+        StroppingDatabase db = new StroppingDatabase(this);
+        db.open();
+
+        db.ResetAllTables();
+
+        db = new StroppingDatabase(this);
+        db.open();
+
+        ArrayList<Ingredient> ingredientArrayList = new ArrayList<>();
+
+        Ingredient i_RedPepper = db.createIngredient("Red Pepper", "number of", 1, 1, 0, 0, 0, 0);
+        Ingredient i_Garlic = db.createIngredient("Garlic", "number of", 1, 1, 0, 0, 0, 0);
+        Ingredient i_RedOnion = db.createIngredient("Red Onion", "number of", 1, 1, 0, 0, 0, 0);
+        Ingredient i_Parsley = db.createIngredient(("Parsley", "grams", 10, 10, 0, 0, 0, 0);
+        Ingredient i_SmallTomato = db.createIngredient("Small Tomato", "number of", 1, 1, 0, 0, 0, 0);
+        Ingredient i_SaladOnions = db.createIngredient("Salad Onions", "number of", 6, 6, 0, 0, 0, 0);
+        Ingredient i_Brown Rice = db.createIngredient("Brown Rice", "grams", 500, 500, 0, 0, 0, 0);
+        Ingredient i_ChickenSC = db.createIngredient("Chicken Stock cube", "number of", 1, 1, 0, 0, 0, 0);
+        Ingredient i_Sausages = db.createIngredient("Turkey Sausages", "number of", 8, 8, 0, 0, 0, 0);
+
+        ingredientArrayList.add(i_RedPepper);
+        ingredientArrayList.add(i_Garlic);
+        ingredientArrayList.add(i_RedOnion);
+        ingredientArrayList.add(i_Parsley);
+        ingredientArrayList.add(i_SmallTomato);
+        ingredientArrayList.add(i_SaladOnions);
+        ingredientArrayList.add(i_Brown);
+        ingredientArrayList.add(i_ChickenSC);
+        ingredientArrayList.add(i_Sausages);
+        db.createRecipe("Spicy Sausage Rice", 2, "", ingredientArrayList);
+        
+        ingredientArrayList.clear();
+        
+        Ingredient i_Onion = db.createIngredient("Onion", "number of", 1, 1, 0, 0, 0, 0);
+        Ingredient i_Mince = db.createIngredient("Turkey Mince", "grams", 500, 500, 0, 0, 0, 0);
+        Ingredient i_ChoppedToms = db.createIngredient("Chopped Tomatoes", "grams", 250, 250, 0, 0, 0, 0);
+        Ingredient i_Spag = db.createIngredient("Spag", "grams", 200, 200, 0, 0, 0, 0);
+        Ingredient i_Carrot = db.createIngredient("Carrot", "number of", 1, 1, 0, 0, 0, 0);
+        Ingredient i_Carrot = db.createIngredient("Carrot", "number of", 1, 1, 0, 0, 0, 0);
+        Ingredient i_Celery = db.createIngredient("Celery", "number of", 1, 1, 0, 0, 0, 0);
+        
+        ingredientArrayList.add(i_RedPepper);
+        i_Onion.setQuantity(2);
+        ingredientArrayList.add(i_Onion);
+        ingredientArrayList.add(i_Garlic);
+        ingredientArrayList.add(i_Mince);
+        i_ChoppedToms.setQuantity(500);
+        ingredientArrayList.add(i_ChoppedToms);
+        ingredientArrayList.add(i_Spag);
+        ingredientArrayList.add(i_Carrot);
+        ingredientArrayList.add(i_Celery);
+        
+        db.createRecipe("Spaghetti Bolognese", 2, "", ingredientArrayList);
+        
+        ingredientArrayList.clear();
+        
+        Ingredient i_BlackBeans = db.createIngredient("Black Beans", "grams", 200, 200, 0, 0, 0, 0);
+        Ingredient i_GroundCumin = db.createIngredient("Ground Cumin", "grams", 10, 10, 0, 0, 0, 0);
+        Ingredient i_GroundCinnamon = db.createIngredient("Ground Cinnamon", "grams", 10, 10, 0, 0, 0, 0);
+        Ingredient i_ChipotlePaste = db.createIngredient("Chipotle Paste", "grams", 50, 50, 0, 0, 0, 0);
+        Ingredient i_CherryToms = db.createIngredient("Cherry Tomatoes", "number of", 30, 30, 0, 0, 0, 0);
+        Ingredient i_RedChilli = db.createIngredient("Red Chilli", "number of", 2, 2, 0, 0, 0, 0);
+        Ingredient i_Apple = db.createIngredient("Apple", "number of", 6, 6, 0, 0, 0, 0);
+        Ingredient i_GemLettuce = db.createIngredient("Gem Lettuce", "number of", 1, 1, 0, 0, 0, 0);
+        Ingredient i_Raddish = db.createIngredient("Raddishes", "number of", 20, 20, 0, 0, 0, 0);
+        Ingredient i_SourCream = db.createIngredient("Sour Cream", "grams", 50, 50, 0, 0, 0, 0);
+        
+        i_Garlic.setQuantity(2);
+        ingredientArrayList.add(i_Garlic);
+        ingredientArrayList.add(i_BlackBeans);
+        ingredientArrayList.add(i_GroundCumin);
+        ingredientArrayList.add(i_GroundCinnamon);
+        ingredientArrayList.add(i_ChipotlePaste);
+        ingredientArrayList.add(i_CherryToms);
+        ingredientArrayList.add(i_RedChilli);
+        ingredientArrayList.add(i_Parsley);
+        i_Apple.setQuantity(1);
+        ingredientArrayList.add(i_Apple);
+        ingredientArrayList.add(i_GemLettuce);
+        i_Raddish.setQuantity(4);
+        ingredientArrayList.add(i_Raddish);
+        ingredientArrayList.add(i_SourCream);
+        
+        db.createRecipe("Veggie Fajitas", 2, "", ingredientArrayList);
+        
+        db.close();
     }
 
     // DEFAULT 'MY FIRST APP' CODE
